1dad889f5fa6c11549df07cc72daadd1d8b5cc1e
JonnerXD <jonner15rhlm@gmail.com>
2023-10-25 18:09:08 -0400

    Se agregan el login y register .php para dar funcion a el sistema de logueo de la pagina ---Gabriel Gonzales

---
+++ login/login.php
@@ -0,0 +1,47 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Conexión a la base de datos (debes configurar las credenciales de tu base de datos)
+    $mysqli = new mysqli('localhost', 'root', '', 'playzone');
+
+    if ($mysqli->connect_error) {
+        die("Error en la conexión a la base de datos: " . $mysqli->connect_error);
+    }
+
+    // Consulta SQL para buscar el usuario por nombre de usuario
+    $sql = "SELECT * FROM usuarios WHERE nombre_usuario = ?";
+
+    // Preparar la consulta
+    $stmt = $mysqli->prepare($sql);
+
+    // Vincular el parámetro
+    $stmt->bind_param("s", $username);
+
+    // Ejecutar la consulta
+    $stmt->execute();
+
+    // Obtener el resultado de la consulta
+    $result = $stmt->get_result();
+
+    if ($result->num_rows === 1) {
+        // El usuario existe, verificar la contraseña
+        $row = $result->fetch_assoc();
+        if (password_verify($password, $row['contrasena'])) {
+            // Contraseña válida, el usuario está autenticado
+            // Puedes redirigir al usuario a su área personal o página de inicio
+            header('Location: hola.html');
+            exit;
+        } else {
+            echo "Contraseña incorrecta";
+        }
+    } else {
+        echo "Usuario no encontrado";
+    }
+
+    // Cerrar la conexión y liberar recursos
+    $stmt->close();
+    $mysqli->close();
+}
+?>
