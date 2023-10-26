64eb0bd5629d9d88ba50bb6e60a1625ff3a09f47
JonnerXD <jonner15rhlm@gmail.com>
2023-10-26 13:42:28 -0400

    Se modifico el codigo de login_procesar.php y register_procesar.php, para mejorar el funcionamiento del logueo y registro. En los archivos dashboard.css y games.css se les agrego imagen de fondo. Y los demas archivos se arreglaron las rutas de los archivos login y registro. --- Jonner Piña

---
+++ login/login_procesar.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Conexión a la base de datos (debes configurar las credenciales de tu base de datos)
+    $mysqli = new mysqli('localhost', 'root', '', 'playzone');
+
+    if ($mysqli->connect_error) {
+        die("Error en la conexión a la base de datos: " . $mysqli->connect_error);
+    }
+
+    // Consulta SQL para buscar el usuario por nombre de usuario
+    $sql = "SELECT * FROM usuarios WHERE nombre_usuario = ?";
+
+    // Preparar la consulta
+    $stmt = $mysqli->prepare($sql);
+
+    // Vincular el parámetro
+    $stmt->bind_param("s", $username);
+
+    // Ejecutar la consulta
+    $stmt->execute();
+
+    // Obtener el resultado de la consulta
+    $result = $stmt->get_result();
+
+    if ($result->num_rows === 1) {
+        // El usuario existe, verificar la contraseña
+        $row = $result->fetch_assoc();
+        if (password_verify($password, $row['contrasena'])) {
+            // Contraseña válida, el usuario está autenticado
+
+            // Almacena el nombre de usuario en la sesión
+            $_SESSION['username'] = $username;
+
+            // Puedes redirigir al usuario a su área personal o página de inicio
+            header('Location: ../dashboard/user_home.php');
+            exit;
+        } else {
+            echo "Contraseña incorrecta<br><br><br>";
+            echo '<a href="login.php">Iniciar Sesión</a>';
+        }
+    } else {
+        echo "Usuario no encontrado<br><br><br>";
+        echo '<a href="login.php">Volver a Intentar</a>';
+    }
+    
+
+    // Cerrar la conexión y liberar recursos
+    $stmt->close();
+    $mysqli->close();
+}
+?>
